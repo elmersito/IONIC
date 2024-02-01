4ec2cf39b839aa7f7c3c3c4af2208d9b68fc846e
elmersito <elmer.meraz.is@unipolidgo.edu.mx>
2024-02-01 14:57:08 -0600

    v_6
    Se agrego la plantilla de Practica masajes y falta que funcione con imagenes

---
+++ Practica_masajes/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Practica_masajes',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
---
+++ Primera_Prueba/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Primera_Prueba',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
